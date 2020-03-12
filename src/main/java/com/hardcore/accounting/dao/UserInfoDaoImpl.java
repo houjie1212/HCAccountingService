b4a8c81f8281611244f94dd4ae3e08722f4df42f
houjie <houjie1212@sina.com>
2020-03-12 22:52:49 +0800

    feat(mysql): connect mysql and query user

--- src/main/java/com/hardcore/accounting/dao/UserInfoDaoImpl.java
+++ src/main/java/com/hardcore/accounting/dao/UserInfoDaoImpl.java
@@ -1,16 +1,24 @@
 package com.hardcore.accounting.dao;
 
+import com.hardcore.accounting.dao.mapper.UserInfoMapper;
 import com.hardcore.accounting.model.persistence.UserInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor(onConstructor =  @__(@Autowired))
 public class UserInfoDaoImpl implements UserInfoDao {
+
+    private final UserInfoMapper userInfoMapper;
+
+//    @Autowired
+//    public UserInfoDaoImpl(UserInfoMapper userInfoMapper) {
+//        this.userInfoMapper = userInfoMapper;
+//    }
 
     @Override
     public UserInfo getUserInfoById(Long userId) {
-        UserInfo userInfo = new UserInfo();
-        userInfo.setId(userId);
-        userInfo.setUsername("test");
-        return userInfo;
+        return userInfoMapper.getUserInfoById(userId);
     }
 }
